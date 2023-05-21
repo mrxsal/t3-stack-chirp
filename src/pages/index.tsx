9f63cd7f0f16ff55ad3c2caf7fc79023e3fe31d2
Saleh Rabbaniy <salehrabbaniy@gmail.com>
2023-05-21 18:51:23 +0200

    add post model and router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,11 @@ import Head from "next/head";
 
 import SignInButton from "~/components/SignInButton";
 import { SignOutButton, useUser } from "@clerk/nextjs";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const user = useUser();
+  const { data } = api.posts.getAll.useQuery();
 
   return (
     <>
@@ -16,6 +18,12 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         {!user.isSignedIn ? <SignInButton /> : <SignOutButton />}
+
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
